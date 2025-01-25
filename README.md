52aebacab9e9aadfced8ca4a71afb2e224c4253c
nickotine-312 <nickbaldassare@gmail.com>
2025-01-25 21:15:37 +0000

    Added break time calculation

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Originally this script identified the breaks by detecting silence, but there was
 
 # TODO
 * Provide the string to remove from the filename (any text from the filename that you do not wish to include in the final file.)
-* Normalize start time for commercial breaks by taking `($end-$start)/2`
+* Normalize start time for commercial breaks by taking `$start + (($end-$start)/2)`
 * Figure out how to dynamically convert the timestamps in the above output file to a format ffmpeg metadata understands
 * Build episode-specific metadata file with chapters and title
 * Encode input file with that metadata file, using a name for the output file with any desired text (from above) removed. 
--- generate_chapters.pl
+++ generate_chapters.pl
@@ -20,24 +20,24 @@ foreach $fname (@fname_list)
 	};
 
 	$length = `ffprobe -i "$dirpath/$fname" -show_format -v quiet | grep duration | cut -d= -f2`;
-	say "DURATION: $length";
 
 	#Convert carriage returns to newlines
 	#(dos2unix didnt work for me so we're doing it the way I know instead.)
 	$err =~ s/\r/\n/g;
-	@lines = split(/\n/, $err);
-	say "#\n";
 
+	@lines = split(/\n/, $err);
 	foreach (@lines) {
-		#Parse the timestamp (in seconds) that blackness starts, and how long it lasts
-		$_ =~ /.*black_start:([0-9\.]+) .*black_duration:([0-9\.]+)/ || next;
-		my ($start, $duration) = ($1, $2);
+		#Parse the timestamp (in seconds) where blackness starts and ends, and how long it lasts
+		$_ =~ /.*black_start:([0-9\.]+) black_end:([0-9\.]+) black_duration:([0-9\.]+)/ || next;
+		my ($start, $end, $duration) = ($1, $2, $3);
 
 		#Skip darkness if in first 5 minutes (theme song), less than 1 second (scene change), or within 4 minutes of end (credits)
 		next if $start < 360;
 	        next if	$duration < 1.0; 
 		next if $length - $start < 300;
 		
-		print("Start: $start | Duration $duration\n");
+		print("Start: $start | End: $end | Duration $duration\n");
+		$break = $start + (($end-$start)/2);
+		print("Proposed break time: $break\n");
 	}
 }
